794b68fc6391eba021b850c41dfa7e7035598f95
q3126286390-sketch <q3126286390@gmail.com>
2026-03-23 22:26:44 +0800

    Update love.js
    4

--- love.js
+++ love.js
@@ -4,8 +4,6 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 
-canvas.width = canvas.clientWidth;
-canvas.height = canvas.clientHeight;
 
 
 let mouse = { x: null, y: null };
